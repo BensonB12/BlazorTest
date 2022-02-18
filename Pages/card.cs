335355ba5d085fb5300169dd2e0cd309cf3a0034
Benson <bensonbird2@gmail.com>
2022-02-17 22:46:05 -0700

    first commit

---
+++ Pages/card.cs
@@ -0,0 +1,58 @@
+namespace blazorBank
+{
+    public abstract class Card
+    {
+        //SuperType VSuperType = global::SuperType.None;
+        public Card(string name, int manaCost)
+        {
+            Name = name;
+            ManaCost = manaCost;
+        }
+
+        public string Name { get; }
+        public decimal ManaCost { get; }
+        public int Loyalty {get ;}
+        //public string SuperType{get{VSuperType;} set{VSuperType = value;}}
+        //public string Art{get; set;};
+    }
+
+    public class CreatureCard : Card
+    {
+        public CreatureCard(string name, int manaCost) : base(name, manaCost)
+        {
+        }
+        public int Power{get; set;}
+        public int Toughness{get; set;}
+    }
+
+    public class Planeswalker : Card
+    {
+        public Planeswalker(string name, int manaCost, int loyalty) : base(name, manaCost)
+        {
+            Loyalty = loyalty;
+        }
+        public int Loyalty{get;}
+    }
+
+    public class Spell : Card
+    {
+        public Spell(string name, int manaCost) : base(name, manaCost)
+        {}
+    }
+
+    public class Land : Card
+    {
+        public Land(string name) : base(name, 0)
+        {
+        }
+    }
+}
+public enum SuperType
+{
+    Basic,
+    Legendary,
+    Snow,
+    Token,
+    None
+}
+
